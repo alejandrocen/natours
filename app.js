49d531bacdb01fd970f41e18f640585575029c3d
Alejandro Cen <alex96.cen@gmail.com>
2019-12-02 15:36:12 -0600

    fix typo: apiError -> appError

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 
-const AppError = require('./utils/apiError')
+const AppError = require('./utils/appError')
 const globalErrorHandler = require('./utils/globalErrorHandler')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
