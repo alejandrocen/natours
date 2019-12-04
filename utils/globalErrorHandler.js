47eb2d084cf5dfd94256c2fd961cf3f87594b24f
Alejandro Cen <alex96.cen@gmail.com>
2019-12-04 15:30:43 -0600

    handle validation error

--- utils/globalErrorHandler.js
+++ utils/globalErrorHandler.js
@@ -1,13 +1,20 @@
 const AppError = require('./appError')
 
 const handleCastError = err => {
-  const message = `Invalid ${err.path}: ${err.value}`
+  const message = `Invalid ${err.path}: ${err.value}.`
   return new AppError(message, 400)
 }
 
 const handleDuplicateFields = err => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
   const message = `Duplicate field value: ${value}.`
+
+  return new AppError(message, 400)
+}
+
+const handleValidationError = err => {
+  const errors = Object.values(err.errors).map(el => el.message)
+  const message = `Invalid input data. ${errors.join('. ')}`
 
   return new AppError(message, 400)
 }
@@ -49,6 +56,7 @@ module.exports = (err, req, res, next) => {
 
     if (error.name === 'CastError') error = handleCastError(error)
     if (error.code === 11000) error = handleDuplicateFields(error)
+    if (error.name === 'ValidationError') error = handleValidationError(error)
 
     sendErrorProd(error, res)
   }
