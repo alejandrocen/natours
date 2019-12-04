0e87d358dae606fbc181d26c6ddeca0eacf7f279
Alejandro Cen <alex96.cen@gmail.com>
2019-12-04 14:41:59 -0600

    handle duplicate database fields

--- utils/globalErrorHandler.js
+++ utils/globalErrorHandler.js
@@ -5,6 +5,13 @@ const handleCastError = err => {
   return new AppError(message, 400)
 }
 
+const handleDuplicateFields = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
+  const message = `Duplicate field value: ${value}.`
+
+  return new AppError(message, 400)
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -39,7 +46,9 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res)
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err }
+
     if (error.name === 'CastError') error = handleCastError(error)
+    if (error.code === 11000) error = handleDuplicateFields(error)
 
     sendErrorProd(error, res)
   }
