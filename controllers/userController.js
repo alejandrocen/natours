c19626b57ade984b138b49d12d587f074219b999
Alejandro Cen <alex96.cen@gmail.com>
2019-12-15 19:14:38 -0600

    get & login users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,23 @@
-exports.getAllUsers = (req, res) => {
-  return res.status(500).json({
-    status: 'error',
-    message: 'Internal servel error'
+const User = require('./../models/userModel')
+const APIFeatures = require('./../utils/apiFeatures')
+const catchAsync = require('./../utils/catchAsync')
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(User.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate()
+
+  const users = await features.query
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      users
+    }
   })
-}
+})
 
 exports.getUser = (req, res) => {
   return res.status(500).json({
